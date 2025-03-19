c8dcc73c652f9cfb73d2b9fc3d311f75391af2ca
amandamorceli <amandamorceli@gmail.com>
2025-03-18 22:29:48 -0300

    Auçla 18/03

--- projetoComBD/routes/web.php
+++ projetoComBD/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Produto;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource("produtos", ProdutoController::class);
